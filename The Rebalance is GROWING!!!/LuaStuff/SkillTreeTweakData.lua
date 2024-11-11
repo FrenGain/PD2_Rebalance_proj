869a2edebc8b55fd48160faf8769f68057e6a692
FrenGain <frengainium@gmail.com>
2024-11-11 18:59:23 -0500

    Delete The Rebalance is GROWING!!! directory

--- The Rebalance is GROWING!!!/LuaStuff/SkillTreeTweakData.lua
+++
@@ -1,5 +0,0 @@
-Hooks:PostHook(SkillTreeTweakData, "init", "rebalance_skill_additions", function(self)
-
-    table.insert(self.skills.pack_mule[2].upgrades, "carry_throw_distance_multiplier_2") --te upragdes in question
-
-end) 
